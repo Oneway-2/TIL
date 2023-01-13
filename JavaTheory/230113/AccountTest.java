87088f5400d6779178bf3de9b67c4764f473587f
Oneway-2 <nuclear0789@naver.com>
2023-01-13 14:14:25 +0900

    override practice

--- JavaTheory/230113/AccountTest.java
+++ JavaTheory/230113/AccountTest.java
@@ -1,3 +1,4 @@
+import acc.Account;
 import acc.SpecialAccount;
 
 public class AccountTest {
@@ -11,5 +12,16 @@ public class AccountTest {
 		sacc.deposit(100000);
 		System.out.println(sacc.info());
 		
+		// upcasting: 자식객체를 부모타입의 레퍼런스 변수에 담을 수 있음.
+		Account acc = new SpecialAccount(101, "고길동", 123000, "Gold");
+		
+		// 근데 acc로 쓸라다보니, SpecialAccount에 있는 grade를 쓸 수가 없음.
+		// 왜냐면 acc엔 그런게 없으니께
+		// 그래서 쓸려면 downcasting 해줘야한다.
+		SpecialAccount downAcc = (SpecialAccount)acc;
+		
+		// 부모에도 있고 자식에도 있는 메소드를 부모의 타입으로 호출 시,
+		// 자식에 있는 메서드가 실행된다. => 다형성ㅎ
+		System.out.println(acc.info());
 	}
 }
